f359d1db8d7159e28c88ea8614ccf049035444d9
Herick Exterkoetter <herickherick47@gmail.com>
2021-02-03 23:01:11 -0300

    :fire: final simple project

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+const home: React.FC = () => {
+  return (
+    <Container>
+      <Title>home</Title>
+    </Container>
+  );
+};
+
+export default home;
